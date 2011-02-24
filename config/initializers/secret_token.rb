b657e982e29bb661ec3743fb8e2ef87772e156bb
Dennis Schmidt <metzeltiger@web.de>
2011-02-24 14:26:29 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HumbLy::Application.config.secret_token = 'f8b133315765b9bd8f63b9a906e1448b7982d86bbc2e68a966df01399de16c298062b93ba57c4332eac8a8bfa5b5c513bbf7314b97ac283a4fc6c3a378b0ea4c'
