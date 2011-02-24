840a375e74771da28aec805c7a806d268a6445ef
Dennis Schmidt <metzeltiger@web.de>
2011-02-24 19:38:21 +1100

    improvements in form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,13 +5,14 @@ var maxLeft, maxTop;
  * prepare the site
  */
 $(document).ready(function() {
+  // let our bees fly a bit smoother
   jQuery.easing.def = "easeInOutSine";
   /*
    * Hide the input (it needs to be visual by default in case someone
    * with deactivated JS comes along).
    * Also, show the bees, since they will only be neat when they're animated.
    */
-  $("#signup").hide();
+  $("#signup_form").hide();
   $("#bumblebees").show();
   
   // get the maximum dimensions to move to for the bees
@@ -22,12 +23,26 @@ $(document).ready(function() {
    * let the markers show their bubble when clicked
    */
   $(".bumblebee").each(function(index, bee) {
+    // spread the beeeees!!!
     $(bee).css('left', $.fn.fly.getRandomMove(maxLeft));
     $(bee).css('top',  $.fn.fly.getRandomMove(maxTop));
+    // and then let them fly
     $(bee).fly();
-    $(bee).mousedown(function() {
-      $("#signup").fadeIn("fast");
-    })
+    // and if they get caught... display the signup page
+    $(bee).mousedown(function() { $("#signup_form").fadeIn("fast"); });
+  });
+  
+  /*
+   * register our submit logic
+   */
+  $("#submitter").click(function(event) {
+    event.preventDefault();
+    $(event.target).parent().hide();
+    $("#email").hide('fast');
+    $("#spinner").show('fast');
+    window.setTimeout("100", function() {
+      $("#spinner").hide('fast');
+    });
   });
 });
 
@@ -42,16 +57,16 @@ $.fn.fly = function() {
   var currentTop  = parseInt(bee.css('top'));
   var moveLeft    = $.fn.fly.getRandomMove(maxDistance) * $.fn.fly.getRandomDirection();
   var moveTop     = $.fn.fly.getRandomMove(maxDistance) * $.fn.fly.getRandomDirection();
-  
+  // bee-escape-detection pre calculations
   var interimLeft = currentLeft + moveLeft;
   var interimTop  = currentTop  + moveTop;
-  
+  // make sure the bees don't leave their beautiful flowers too far
   var newLeft     = (interimLeft > maxLeft || interimLeft < 0) ? (currentLeft - moveLeft) : interimLeft;
   var newTop      = (interimTop  > maxTop  || interimTop  < 0) ? (currentTop  - moveTop)  : interimTop;
-  
+  // let the bees fleeeeee... erm... I mean fly :)
   bee.animate({
-    left: [newLeft, 'swing'],
-    top:  [newTop, 'swing'],
+    left: newLeft,
+    top:  newTop,
   }, 100, function() {bee.fly()});
 };
 
@@ -62,6 +77,9 @@ $.fn.fly.getRandomMove = function(max) {
   return parseInt(Math.floor(Math.random() * max));
 };
 
+/*
+ * randomly return a value of 1 or -1
+ */
 $.fn.fly.getRandomDirection = function() {
   return Math.pow(-1, parseInt(Math.floor(Math.random() * 10)));
 };
